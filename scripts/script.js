3b59f9e9d4e683eae15fa2ac53cb85b3e4ba1f58
mohammad reza <mrghkh12@gmail.com>
2024-07-19 17:13:24 +0330

    feat(script.js): add click event on control btn

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,15 @@ const $ = document
 let [soconds, minutes, hours] = [0,0,0];
 let timerElem = $.querySelector('.timer')
 let ctrlBtns = $.querySelector('.ctrlBtns')
+
+ctrlBtns.addEventListener('click', e => {
+    if(e.target.src){
+        if(e.target.src.includes('start')){
+            console.log('start');
+        }else if(e.target.src.includes('stop')){
+            console.log('stop');
+        }else if(e.target.src.includes('reset')){
+            console.log('reset');
+        }
+    }
+})
