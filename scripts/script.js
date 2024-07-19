9295274dce0cc1ecf38fc5baddd2cd7f15619481
mohammad reza <mrghkh12@gmail.com>
2024-07-19 17:38:27 +0330

    feat(script.js): create strat timer fuuction

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,12 @@ const $ = document
 let [soconds, minutes, hours] = [0,0,0];
 let timerElem = $.querySelector('.timer')
 let ctrlBtns = $.querySelector('.ctrlBtns')
+let timeOut = null
 
 ctrlBtns.addEventListener('click', e => {
     if(e.target.src){
         if(e.target.src.includes('start')){
-            console.log('start');
+            stsrtTimer()
         }else if(e.target.src.includes('stop')){
             console.log('stop');
         }else if(e.target.src.includes('reset')){
@@ -15,3 +16,19 @@ ctrlBtns.addEventListener('click', e => {
         }
     }
 })
+
+function stsrtTimer(){
+    if(soconds === 0){
+        timeOut = setInterval(()=>{
+            soconds++
+            if(soconds == 60) soconds=0 , minutes++;
+            if(minutes == 60) minutes=0 , hours++;
+    
+            let socondsStr = soconds < 10 ? '0' + soconds : soconds
+            let minutesStr = minutes < 10 ? '0' + minutes : minutes
+            let hoursStr = hours < 10 ? '0' + hours : hours
+        
+            timerElem.innerHTML = hoursStr + ':' + minutesStr + ':' + socondsStr
+        }, 1000)
+    }
+}
